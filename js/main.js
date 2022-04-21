ffa4d1012461c77f4deab3b07307b7db4362f767
dd-obua <dd.obua@gmail.com>
2022-04-21 19:58:13 +0300

    Add about page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-const hamburger = document.querySelector('.hamburger');
-const close = document.querySelector('.close');
-const menu = document.querySelector('.nav-menu');
+// const hamburger = document.querySelector('.hamburger');
+// const close = document.querySelector('.close');
+// const menu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener('click', () => {
-  hamburger.style.display = 'none';
-  menu.style.display = 'block';
-  close.style.display = 'block';
-});
+// hamburger.addEventListener('click', () => {
+//   hamburger.style.display = 'none';
+//   menu.style.display = 'block';
+//   close.style.display = 'block';
+// });
 
-close.addEventListener('click', () => {
-  hamburger.style.display = 'block';
-  menu.style.display = 'none';
-  close.style.display = 'none';
-});
+// close.addEventListener('click', () => {
+//   hamburger.style.display = 'block';
+//   menu.style.display = 'none';
+//   close.style.display = 'none';
+// });
